ebf869ed1a4549ea64277a9d8322ddd1b8114a03
adoka <adokanchi@gmail.com>
2025-02-26 09:43:25 -0800

    f

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 /**
  * Plagiarism Checker
  * A tool for finding the longest shared substring between two documents.
@@ -15,49 +14,18 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-        final int LARGEST_CHAR = 8218;
         int n1 = doc1.length(), n2 = doc2.length();
         int[][] dp = new int[n1 + 1][n2 + 1];
-
-        // indices.get(c) is a sorted list of all indices of doc1 with the character c.
-        ArrayList<ArrayList<Integer>> indices = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < LARGEST_CHAR; i++) {
-            indices.add(new ArrayList<Integer>());
-        }
-        for (int i = 0; i < n1; i++) {
-            indices.get(doc1.charAt(i)).add(i);
-        }
-
         for (int i = 1; i <= n1; i++) {
             for (int j = 1; j <= n2; j++) {
-                // targetIdx is the index of the last character of doc1 that matches target, before the ith character.
-                char target = doc2.charAt(j - 1);
-                int targetIdx = findLargest(indices.get(target), i - 1);
-
-                // Exclude next character of doc2
-                dp[i][j] = dp[i][j-1];
-                // Include next character of doc2
-                if (targetIdx != -1) {
-                    dp[i][j] = Math.max(dp[i][j - 1], 1 + dp[targetIdx][j - 1]);
+                if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
         return dp[n1][n2];
-    }
-
-    // Returns the largest number in (sorted) list less than or equal to max using binary search.
-    private static int findLargest(ArrayList<Integer> list, int max) {
-        int left = 0, right = list.size() - 1, largest = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (list.get(mid) <= max) {
-                largest = list.get(mid);
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-        return largest;
     }
 }
